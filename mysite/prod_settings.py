71d47a102a37da2f6751cafd9586a1ecd7477d38
jeromepacman <jeromepacman@gmail.com>
2020-11-16 12:31:18 +0100

    prod settings

--- mysite/prod_settings.py
+++ mysite/prod_settings.py
@@ -1,14 +1,13 @@
-
 from mysite.settings import *
 
 DEBUG = False
 
 TEMPLATE_DEBUG = False
 
-SECRET_KEY = os.environ['SECRET_KEY']
+SECRET_KEY = get_env_variable('SECRET_KEY')
 
-ALLOWED_HOSTS = ['cryptonews.herokuapp.com', 'www.cryptonews.herokuapp.com']
+ALLOWED_HOSTS = ['cryptonews.herokuapp.com', 'www.cryptonews.herokuapp']
 
-
+SECURE_SSL_REDIRECT = True
 
 
